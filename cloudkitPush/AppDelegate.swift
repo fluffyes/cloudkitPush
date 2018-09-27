9bb4eceba241e22c5267f50d9977894243235472
Axel <soulchildsince2013@gmail.com>
2018-09-28 00:10:55 +0800

    Added more comment documentation in code
    Added more comment documentation in code

--- cloudkitPush/AppDelegate.swift
+++ cloudkitPush/AppDelegate.swift
@@ -65,18 +65,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         
         // Create a subscription to the 'Notifications' Record Type in CloudKit
-        // User will receive a notification when a new record is created in CloudKit
+        // User will receive a push notification when a new record is created in CloudKit
+        // Read more on https://developer.apple.com/library/archive/documentation/DataManagement/Conceptual/CloudKitQuickStart/SubscribingtoRecordChanges/SubscribingtoRecordChanges.html
+        
+        // The predicate lets you define condition of the subscription, eg: only be notified of change if the newly created notification start with "A"
+        // the TRUEPREDICATE means any new Notifications record created will be notified
         let subscription = CKQuerySubscription(recordType: "Notifications", predicate: NSPredicate(format: "TRUEPREDICATE"), options: .firesOnRecordCreation)
         
         // Here we customize the notification message
         let info = CKNotificationInfo()
         
+        // this will use the 'title' field in the Record type 'notifications' as the title of the push notification
         info.titleLocalizationKey = "%1$@"
         info.titleLocalizationArgs = ["title"]
+        
+        // if you want to use multiple field combined for the title of push notification
+        // info.titleLocalizationKey = "%1$@ %2$@" // if want to add more, the format will be "%3$@" and so on
+        // info.titleLocalizationArgs = ["title", "subtitle"]
+        
+        // this will use the 'content' field in the Record type 'notifications' as the title of the push notification
         info.alertLocalizationKey = "%1$@"
         info.alertLocalizationArgs = ["content"]
+        
+        // increment the red number count on the top right corner of app icon
         info.shouldBadge = true
+        
+        // use system default notification sound
         info.soundName = "default"
+        
         subscription.notificationInfo = info
         
         // Save the subscription to Public Database in Cloudkit
